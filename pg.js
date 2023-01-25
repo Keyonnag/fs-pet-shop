d50af8b48294e62074f157ec0bbc4efb7bafb0bd
Keyonna Garfine <keyonnagarfine@Keyonnas-Air.lan>
2023-01-25 16:50:44 -0600

    Final commit

--- pg.js
+++ pg.js
@@ -10,12 +10,3 @@ const client = new Pool({
 
 module.exports = client;
  
-
-// // async/await
-// try {
-//     const res = await client.query(text, values)
-//     console.log(res.rows[0])
-//     // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
-//   } catch (err) {
-//     console.log(err.stack)
-//   }
--- restfulServer.js
+++ restfulServer.js
@@ -6,7 +6,10 @@ const app = express()
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
+
+ 
 app.route('/pets')
+    // Get all
     .get( async (req, res) => {
         try {
             const result = await client.query('SELECT * FROM pets');
@@ -15,6 +18,7 @@ app.route('/pets')
             res.status(500).type('text/plain').send(error)
         }      
     })
+    // Create
     .post(async (req, res) => {
         let { body }  = req
         if (createPetValidation(body)) {
@@ -29,6 +33,7 @@ app.route('/pets')
         }        
     })
 app.route('/pets/:id')
+    // Get one
     .get( async (req, res) => {
         const id = req.params.id
         try {
@@ -38,30 +43,53 @@ app.route('/pets/:id')
         res.status(500).type('text/plain').send(error)
     }      
 })   
+    // Edit one
     .patch(async (req, res) => {
-        const id = req.params.id
-        let { body }  = req
-        if (patchPetValidation(body)) {
+        const { id } = req.params
+        let { body } = req
+            if (patchPetValidation(body)) {
+                try {
+                const result = await client.query(`SELECT * FROM pets WHERE pet_id = ${id}`);
+                    if (result.rows.length === 0) {
+                        res.status(404).type('text/plain').send('Not found')
+                    } else {
+                        for (key in body) {
+                            await client.query(`UPDATE pets SET ${key} = '${body[key]}' WHERE pet_id = ${id}`); 
+                        }
+                    const updatedPet = await client.query(`SELECT * FROM pets WHERE pet_id = ${id}`);
+                    res.status(200).type('application/JSON').json(updatedPet.rows) 
+                    }          
+                } catch (error) {
+                    res.status(500).type('text/plain').send(error)
+                }
+            } else {
+                res.status(400).type('text/plain').send("Bad Request")
+        }                
+    })
+    // Edit one
+    .put(async (req,res) => {
+        const { age, name, kind } = req.body
+        const { id } = req.params
+        if (createPetValidation(req.body)) {
             try {
-                const result = await client.query('UPDATE pets SET name = $1  WHERE pet_id = $2', [body.name, id])
-                res.status(200).type('application/json').json(body)
+                const result = await client.query('UPDATE pets SET age = $1, name = $2, kind = $3 WHERE pet_id = $4', [ age, name, kind, id])
+                res.status(201).type('application/json').json('Success Pet updated')
             } catch(error) {
-                res.status(500).type('text/plain').send(error) 
+                res.status(500).type('text/plain').send(error)  
             };
         } else {
             res.status(400).type('text/plain').send("Bad Request")
-        }        
+        }
     })
     .delete(async (req, res) => {
         const id = req.params.id
         try {
             const result = await client.query('DELETE FROM pets WHERE pet_id = $1', [id])
             res.status(200).type('application/json').json('PET DELETE AT $1', [id])
-    } catch(error) {
-        res.status(500).type('text/plain').send(error)
-    };
+        } catch(error) {
+            res.status(500).type('text/plain').send(error)
+        }
     })
-
 // Responds with error if no routes are hit
 app.use((req, res) => {
     res.status(404).type('text/plain').send('Not found')
@@ -108,3 +136,4 @@ function patchPetValidation(obj) {
         return true
     } else return false
 } 
+
